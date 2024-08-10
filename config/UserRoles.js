b1b7de2dccdfc30fa5220608f6f5c63620a9b8fb
Dennis Appah <dev.dennisappah@gmail.com>
2024-08-10 04:49:02 +0000

    Enabled file uploads and added authorisation

--- config/UserRoles.js
+++ config/UserRoles.js
@@ -1,4 +1,4 @@
-export const userRoles = [
+export const roles = [
     {
         role: "user",
         permission: [
---
+++ middleware/authz.js
@@ -0,0 +1,22 @@
+import { roles } from "../config/UserRoles";
+
+export const hasPermission = (permission) => {
+    return async (req, res, next) => {
+        try {
+            // Get user id from session or request
+            const id = req.session?.user?.id || req?.user?.id;
+            // Find user by id
+            const user = await UserModel.findById(id);
+            // Find user role with permissions
+            const userRole = roles.find(element => element.role === user.role);
+            // Use role to check if user has permission
+            if (userRole && userRole.permissions.includes(permission)) {
+                next();
+            } else {
+                res.status(403).json('User Not Authorized!');
+            }
+        } catch (error) {
+            next(error);
+        }
+    }
+}
